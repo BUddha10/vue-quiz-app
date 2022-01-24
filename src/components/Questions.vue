234bd81d00054697725c8a5535b2c3e8ff6975a1
buddha <buddhatamang1000@gmail.com>
2022-01-24 22:37:46 +0545

    make question section functional

---
+++ src/components/Questions.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div
+        class="bar"
+        :style="{
+          width: `${(questionAnswered / questions.length) * 100}%`,
+        }"
+      ></div>
+      <div class="status">
+        {{ questionAnswered }} out of {{ questions.length }} questions answered
+      </div>
+    </div>
+    <div
+      class="single-question"
+      v-for="(question, qIndex) in questions"
+      :key="question.q"
+      v-show="questionAnswered == qIndex"
+    >
+      <div class="question">
+        {{ question.q }}
+      </div>
+      <div class="answers">
+        <div
+          class="answer"
+          v-for="answer in question.answers"
+          :key="answer.text"
+          @click="selectAnswer(answer.is_correct)"
+        >
+          {{ answer.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Questions",
+  props: ["questions", "questionAnswered"],
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit("selectedAnswer", is_correct);
+    },
+  },
+};
+</script>
